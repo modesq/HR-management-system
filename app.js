c51a165a7749de4084a19ec363b25639da550202
mahmoud qunbor <modesq.99q@gmail.com>
2022-08-08 00:38:56 +0300

    without DOM

--- app.js
+++ app.js
@@ -1,155 +1,58 @@
 'use strict'
+
+
 var counter = 1000;
 
-let employee1 = {
-    employeeID: function generateEmpID() {
-        employeeID = counter;
-        counter++;
-        return this.counter;
-    },
-    fullName: "Ghazi Samer",
-    department: "Administration",
-    level: "Senior",
-    imgLink: "./images/avatar-cd104213bcc8422a34755aa323557594.jpg",
-    salary: function generateSalary() {
-        let min = 1500;
-        let max = 2000;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-};
+function Employee(fullName, department, level, imgLink) {
+    this.fullName = fullName;
+    this.department = department;
+    this.level = level;
+    this.imgLink = imgLink;
+    this.salary = generateSalary(level);
+    this.employeeID = generateEmpID();
+}
+
+const employee1 = new Employee("Ghazi Samer", "Administration", "Senior", "./images/avatar-cd104213bcc8422a34755aa323557594.jpg");
+const employee2 = new Employee("Lana Ali", "Finance", "Senior", "./images/avatar-c91d5e4db150c06a700b6607888b3fad.jpg");
+const employee3 = new Employee("Tamara Ayoub", "Marketing", "Senior", "./images/avatar-d89bf695c57f9669f52f7bf135be4810.jpg");
+const employee4 = new Employee("Safi Walid", "Administration", "Mid-Senior", "./images/avatar-cd104213bcc8422a34755aa323557594.jpg");
+const employee5 = new Employee("Omar Zaid", "Development", "Senior", "./images/avatar-bdc03af8212ca95d8506b95515cf2b1d.jpg");
+const employee6 = new Employee("Rana Saleh", "Development", "Junior", "./images/avatar-aff6c6097a7c9d198d60505ecfa8feb0.jpg");
+const employee7 = new Employee("Hadi Ahmad", "Finance", "Mid-Senior", "./images/avatar-a9222bb88c1bcaeebb82fffc34001774.jpg");
+
+
 console.log("Employee name: " + employee1.fullName + "\n" + "Employee salary: " + employee1.salary);
-
-let employee2 = {
-    employeeID: function generateEmpID() {
-        employeeID = counter;
-        counter++;
-        console.log(this.counter);
-    },
-    fullName: "Lana Ali",
-    department: "Finance",
-    level: "Senior",
-    imgLink: "./images/avatar-c91d5e4db150c06a700b6607888b3fad.jpg",
-    salary: function generateSalary() {
-        let min = 1500;
-        let max = 2000;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-};
 console.log("Employee name: " + employee2.fullName + "\n" + "Employee salary: " + employee2.salary);
-
-let employee3 = {
-    employeeID: function generateEmpID() {
-        employeeID = counter;
-        counter++;
-        console.log(this.counter);
-    },
-    fullName: "Tamara Ayoub",
-    department: "Marketing",
-    level: "Senior",
-    imgLink: "./images/avatar-d89bf695c57f9669f52f7bf135be4810.jpg",
-    salary: function generateSalary() {
-        let min = 1500;
-        let max = 2000;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-};
 console.log("Employee name: " + employee3.fullName + "\n" + "Employee salary: " + employee3.salary);
-
-let employee4 = {
-    employeeID: function generateEmpID() {
-        employeeID = counter;
-        counter++;
-        console.log(this.counter);
-    },
-    fullName: "Safi Walid",
-    department: "Administration",
-    level: "Mid-Senior",
-    imgLink: "./images/avatar-cd104213bcc8422a34755aa323557594.jpg",
-    salary: function generateSalary() {
-        let min = 1000;
-        let max = 1500;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-};
 console.log("Employee name: " + employee4.fullName + "\n" + "Employee salary: " + employee4.salary);
-
-let employee5 = {
-    employeeID: function generateEmpID() {
-        employeeID = counter;
-        counter++;
-        console.log(this.counter);
-    },
-    fullName: "Omar Zaid",
-    department: "Development",
-    level: "Senior",
-    imgLink: "./images/avatar-bdc03af8212ca95d8506b95515cf2b1d.jpg",
-    salary: function generateSalary() {
-        let min = 1500;
-        let max = 2000;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-};
 console.log("Employee name: " + employee5.fullName + "\n" + "Employee salary: " + employee5.salary);
-
-let employee6 = {
-    employeeID: function generateEmpID() {
-        employeeID = counter;
-        counter++;
-        console.log(this.counter);
-    },
-    fullName: "Rana Saleh",
-    department: "Development",
-    level: "Junior",
-    imgLink: "./images/avatar-aff6c6097a7c9d198d60505ecfa8feb0.jpg",
-    salary: function generateSalary() {
-        let min = 500;
-        let max = 1000;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-};
 console.log("Employee name: " + employee6.fullName + "\n" + "Employee salary: " + employee6.salary);
-
-let employee7 = {
-    employeeID: function generateEmpID() {
-        employeeID = counter;
-        counter++;
-        console.log(this.counter);
-    },
-    fullName: "Hadi Ahmad",
-    department: "Finance",
-    level: "Mid-Senior",
-    imgLink: "./images/avatar-a9222bb88c1bcaeebb82fffc34001774.jpg",
-    salary: function generateSalary() {
-        let min = 1000;
-        let max = 1500;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-};
 console.log("Employee name: " + employee7.fullName + "\n" + "Employee salary: " + employee7.salary);
 
-// function generateEmpID() {
-//     employee1.employeeID = counter;
-//     counter++;
-//     console.log(this.counter);
-//     return employee.employeeID;
+// Employee.prototype.render = function() {
+//     document.getElementsByClassName("item1").write;
 // }
 
-function generateSalary() {
-    if (employee.level === "Senior") {
+// employee1.render();
+
+function generateSalary(level) {
+    if (level === "Senior") {
         let min = 1500;
         let max = 2000;
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    if (employee.level === "Mid-Senior") {
+    if (level === "Mid-Senior") {
         let min = 1000;
         let max = 1500;
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    if (employee.level === "Junior") {
+    if (level === "Junior") {
         let min = 500;
         let max = 1000;
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 }
 
-//  console.log("Employee name: " + employee.fullName + "\n" + "Employee salary: " + employee.salary);
+function generateEmpID() {
+    return counter++;
+}
